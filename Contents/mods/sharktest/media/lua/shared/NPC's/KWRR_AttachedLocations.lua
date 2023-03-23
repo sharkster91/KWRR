5c65a587d1fcc317c89db10c56212f62f2ee8c0a
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2023-03-23 18:16:17 -0400

    Continued

--- Contents/mods/sharktest/media/lua/shared/NPC's/KWRR_AttachedLocations.lua
+++ Contents/mods/sharktest/media/lua/shared/NPC's/KWRR_AttachedLocations.lua
@@ -4,7 +4,7 @@
 
 local group = AttachedLocations.getGroup("Human")
 
-group:getOrCreateLocation("Katana in Sheath"):setAttachmentName("Katana_Sheath")
+group:getOrCreateLocation("Aerondight in Sheath"):setAttachmentName("Aerondight_Sheath")
 
 if getDebug() then
 	group:getOrCreateLocation("OnBack"):setAttachmentName("back")
