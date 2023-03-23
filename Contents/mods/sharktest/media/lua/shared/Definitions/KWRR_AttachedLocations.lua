1b722d0aa0fc46fd07720269bdc709fe0e99a5e9
sharkster91 <79799951+sharkster91@users.noreply.github.com>
2023-03-22 23:48:01 -0400

    Again

---
+++ Contents/mods/sharktest/media/lua/shared/Definitions/KWRR_AttachedLocations.lua
@@ -0,0 +1,9 @@
+--***********************************************************
+--**                    THE INDIE STONE                    **
+--***********************************************************
+
+local group = AttachedLocations.getGroup("Human")
+
+group:getOrCreateLocation("Webbing Rope Left"):setAttachmentName("Webbing_Rope_Left")
+
+
